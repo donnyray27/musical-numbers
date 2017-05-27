434a9e21704ee8a3026a1da2c90cecf868c26b36
Donathan Raymond <bepop@Donathans-MacBook-Pro.local>
2017-05-27 11:25:35 -0400

    created the dropdown of notes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      changeNote: false,
+      selectedKey: ''
+    }
+    this.handleNoteChange = this.handleNoteChange.bind(this)
+  }
+
+  handleNoteChange(event){
+    let keyChange = event.target.value
+    let flatKeys = [ "F", "Bb", "Eb", "Ab", "Db", "Gb" ]
+    let sharpKeys = ["G", "A", "B", "D", "E"]
+    this.setState({selectedKey: keyChange})
+
+  }
   render() {
+    let sharps = [
+      "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
+    ]
+
+    let flats = [
+      "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"
+    ]
+
+    let dropdown = [
+      "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"
+    ]
+
+    let options = dropdown.map(option => {
+      return(
+      <option key={dropdown.indexOf(option)} value={option}>{option}</option>
+      )
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <select onChange={this.handleNoteChange}>
+          {options}
+        </select>
       </div>
     );
   }
